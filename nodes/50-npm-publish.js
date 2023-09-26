93945d35d40cdcf133a789f0c870f3cccb312c3d
Gerrit Riessen <gerrit@openmindmap.org>
2023-09-26 12:09:36 +0200

    npm publish working

--- nodes/50-npm-publish.js
+++ nodes/50-npm-publish.js
@@ -5,14 +5,72 @@ module.exports = function (RED) {
         var node = this;
         var cfg = config;
 
+        const libpub = require('libnpmpublish');
+
         node.on('close', function () {
             node.status({});
         });
 
         node.on("input", function (msg, send, done) {
-            send(msg);
-            done();
+            RED.util.evaluateNodeProperty(cfg.authToken, cfg.authTokenType, node, msg, (err, result) => {
+                if (err || result.trim() == "") {
+                    node.status({
+                        fill: "red",
+                        shape: "dot",
+                        text: "Failed, no AUTH TOKEN provided."
+                    });
+
+                    msg.error = err;
+                    done("failed to get auth token", msg)
+                    return;
+                }
+
+                var auth_token = result;
+                var tarball = Buffer.from(msg.payload)
+
+                var manifest = JSON.parse(msg.contents.filter((d) => {
+                    return d.name == "package.json"
+                })[0].contents);
+
+                var opts = {
+                    userAgent: "node-red-contrib-nodedev@1.1.1",
+                    access: "public",
+                    otp: msg.npmotp || cfg.otp,
+                    authToken: auth_token,
+                    '//registry.npmjs.org/:_authToken': auth_token,
+                };
+
+                var userscope = manifest.name.split("/")[0];
+                opts[userscope + ":registry"] = "https://registry.npmjs.org"
+
+                if (cfg.action == "publish") {
+                    libpub.publish(
+                        manifest, tarball, opts
+                    ).then((data) => {
+                        msg.payload = JSON.stringify(data);
+                        send(msg)
+                        done()
+                    }).catch((exp) => {
+                        msg.error = exp;
+                        done("publish failed", msg)
+                    })
+                }
+
+                if (cfg.action == "unpublish") {
+                    libpub.unpublish(
+                        manifest.name, opts
+                    ).then((data) => {
+                        msg.payload = JSON.stringify(data);
+                        send(msg)
+                        done()
+                    }).catch((exp) => {
+                        msg.error = exp;
+                        done("publish failed", msg)
+                    })
+                }
+            })
         });
     }
+    
     RED.nodes.registerType("NpmPublish", NpmPublishFunctionality);
 }
