21998ebedf0391c196e815bca193ac50c97333c0
Gerrit Riessen <gerrit@openmindmap.org>
2023-10-16 22:11:15 +0200

    readme update

--- nodes/templates/tmplsidebarnode.js
+++ nodes/templates/tmplsidebarnode.js
@@ -2,5 +2,26 @@ module.exports = function (RED) {
   function Config{{ node.name }}Functionality(config) {
     RED.nodes.createNode(this, config)
   }
+  
   RED.nodes.registerType('{{ node.name }}Cfg', Config{{ node.name }}Functionality);
+
+{{ #node.frt2bakcomm }}
+  RED.httpAdmin.post("/{{ node.name }}Cfg",
+    RED.auth.needsPermission("{{ node.name }}Cfg.write"),
+    (req, res) => {
+      try {
+        if (req.body) {
+          res.status(200).send({
+            "status": "ok",
+            "value": req.body.data
+          })
+        } else {
+          res.sendStatus(404);
+        }
+      } catch (err) {
+        console.error(err);
+        res.status(500).send(err.toString());
+      }
+    });
+{{ /node.frt2bakcomm }}
 }
