40fcc59e8abcda152584d1260661680fa0500da9
Gerrit Riessen <gerrit@openmindmap.org>
2024-10-16 12:15:23 +0200

    added github & npm tokens to the NodeDevOps node

--- nodes/05-node-dev-ops.js
+++ nodes/05-node-dev-ops.js
@@ -11,21 +11,40 @@ module.exports = function (RED) {
 
         node.on("input", function (msg, send, done) {
             msg.commit_message = msg.githubmessage;
-            
+
             if (msg.randompackagename) {
-                msg.pversion = ( 
-                    Math.random().toString().substring(2).substring(2, 3) + "." + 
-                    Math.random().toString().substring(2).substring(2, 3) + "." + 
+                msg.pversion = (
+                    Math.random().toString().substring(2).substring(2, 3) + "." +
+                    Math.random().toString().substring(2).substring(2, 3) + "." +
                     Math.random().toString().substring(2).substring(2, 5).replace(/^0/, '1')
                 )
             }
 
-            if (msg.randompackagename && (msg.gitcommit || msg.npmpublish || msg.npmunpublish) ) {
+            if (msg.randompackagename && (msg.gitcommit || msg.npmpublish || msg.npmunpublish)) {
                 done("Cannot randomise package and perform GitHub or NPM operation", msg)
                 return
             }
-            send(msg);
-            done();
+
+            RED.util.evaluateNodeProperty(cfg.npmtoken, cfg.npmtokenType, node, msg, (errNpm, resultNpm) => {
+                RED.util.evaluateNodeProperty(cfg.githubtoken, cfg.githubtokenType, node, msg, (errGitHub, resultGitHub) => {
+                    if (errNpm || !resultNpm) {
+                        if (msg.npmpublish || msg.npmunpublish) {
+                            done("Cannot perform NPM operation without NPM auth token", msg)
+                            return
+                        }
+                    } else { msg.npmauthtokenvalue = resultNpm }
+
+                    if (errGitHub || !resultGitHub) {
+                        if (msg.gitcommit) {
+                            done("Cannot perform GitHub commit without GitHub auth token", msg)
+                            return
+                        }
+                    } else { msg.githubauthtokenvalue = resultGitHub }
+
+                    send(msg);
+                    done();
+                })
+            })
         });
     }
 
@@ -37,7 +56,7 @@ module.exports = function (RED) {
             var node = RED.nodes.getNode(req.params.id);
             if (node != null) {
                 try {
-                    if (req.body && node.type == "NodeDevOps") {                        
+                    if (req.body && node.type == "NodeDevOps") {
                         node.receive(req.body);
                         res.status(200).send({ status: "ok" })
                     } else {
